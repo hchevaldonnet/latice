58c9e819f838015b5092ddd17756e87d007bed9e
hchevaldonnet <hubert.chevaldonnet@etu.unilim.fr>
2025-04-01 17:15:39 +0200

    ajout mention collaborateur 1

--- src/main/java/latice/application/LaticeApplicationConsole.java
+++ src/main/java/latice/application/LaticeApplicationConsole.java
@@ -5,6 +5,7 @@ public class LaticeApplicationConsole {
 	public static void main(String[] args) {
 		System.out.println("-------------------------------------------------------");
 		System.out.println("-- Bienvenue dans notre magnifique jeu de latice ! --");
+		System.out.println("-- développé par Hubert                            --");
 		System.out.println("-------------------------------------------------------");
 	}
 
