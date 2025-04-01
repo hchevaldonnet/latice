f917c188281a6336f2474096a47d437bf5274065
BEGUE Mattéo <begue.matteo24@gmail.com>
2025-04-01 17:21:45 +0200

    ajout collaborateur 2 (le goat)

--- src/main/java/latice/application/LaticeApplicationConsole.java
+++ src/main/java/latice/application/LaticeApplicationConsole.java
@@ -5,7 +5,8 @@ public class LaticeApplicationConsole {
 	public static void main(String[] args) {
 		System.out.println("-------------------------------------------------------");
 		System.out.println("-- Bienvenue dans notre magnifique jeu de latice ! --");
-		System.out.println("-- développé par Ferdina                            --");
+		System.out.println("-- développé par Ferdina                           --");
+		System.out.println("-- et par Mattéo                                   --");
 		System.out.println("-------------------------------------------------------");
 	}
 
